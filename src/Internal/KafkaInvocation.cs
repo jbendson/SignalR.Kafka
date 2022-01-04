787ee99c0af5a431e199970d730125a1cc9c496d
Josh Bendson <jbendson@gmail.com>
2022-01-04 16:51:15 -0600

    cleanup

--- src/Internal/KafkaInvocation.cs
+++ src/Internal/KafkaInvocation.cs
@@ -7,15 +7,8 @@ namespace Ascentis.SignalR.Kafka.Internal;
 
 internal readonly struct Invocation
 {
-    /// <summary>
-    /// Gets a list of connections that should be excluded from this invocation.
-    /// May be null to indicate that no connections are to be excluded.
-    /// </summary>
     public IReadOnlyList<string>? ExcludedConnectionIds { get; }
 
-    /// <summary>
-    /// Gets the message serialization cache containing serialized payloads for the message.
-    /// </summary>
     public SerializedHubMessage Message { get; }
 
     public Invocation(SerializedHubMessage message, IReadOnlyList<string>? excludedConnectionIds)
