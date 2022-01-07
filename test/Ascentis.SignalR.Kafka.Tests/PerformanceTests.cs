3f9819f8cb969d10c44773a0fffbbc35e840fef3
Josh Bendson <jbendson@gmail.com>
2022-01-07 15:31:02 -0600

    Performance testing baseline for SendConnection to client connected to remote server

--- test/Ascentis.SignalR.Kafka.Tests/PerformanceTests.cs
+++ test/Ascentis.SignalR.Kafka.Tests/PerformanceTests.cs
@@ -12,10 +12,10 @@ namespace Ascentis.SignalR.Kafka.IntegrationTests;
 [TestClass]
 public class PerformanceTests
 {
-    private static readonly int[] _ports = new int[] { 6010 };
+    private static readonly int[] _ports = new int[] { 6010, 6011 };
     private const int RpcWait = 1000;
     private const int StartupWait = 5000;
-    private const int ConnectionCount = 5;
+    private const int ConnectionCount = 6;
     private static List<Process> _servers = new();
     private static readonly SemaphoreSlim _testLock = new (1);
     private ConnectionManager _connectionManager;
@@ -147,19 +147,90 @@ public class PerformanceTests
     [TestMethod]
     public async Task SendAll()
     {
-        const int messagesPerConnection = 1000;
+        const int messagesPerConnection = 10000;
         await PrimeServers();
         var startTime = DateTime.UtcNow;
-        var tasks = new List<Task>(ConnectionCount);
+        var tasks = new List<Task>();
+        tasks.Add(CheckLifetimeEnqueued(ConnectionCount * ConnectionCount * messagesPerConnection));
         foreach (var connection in _connectionManager)
             for (var i = 0; i < messagesPerConnection; i++)
                 tasks.Add(connection.InvokeAsync("SendAll", $"{_message} {i}"));
 
-        while (_messageManager.LifetimeEnqueued() < ConnectionCount * ConnectionCount * messagesPerConnection && (DateTime.UtcNow - startTime).TotalMilliseconds < ConnectionCount * RpcWait * 10)
-            await Task.Delay(10);
-
         await Task.WhenAll(tasks);
+        TestContext.WriteLine($"Received messages/sec: {_messageManager.LifetimeEnqueued() / (DateTime.UtcNow - startTime).TotalSeconds}");
         Assert.AreEqual(ConnectionCount * ConnectionCount * messagesPerConnection, _messageManager.LifetimeEnqueued());
         TestContext.WriteLine($"Elapsed ms: {(DateTime.UtcNow - startTime).TotalMilliseconds}");
+    }
+
+    [TestMethod]
+    public async Task SendConnection_Pair()
+    {
+        if (ConnectionCount < 2)
+            Assert.Inconclusive("Test requires 2 connections");
+
+        const int messagesPerConnection = 100000;
+        await PrimeServers();
+        var startTime = DateTime.UtcNow;
+        var tasks = new List<Task>();
+        var invocationConnection = _connectionManager.First();
+        var receivingConnection = _connectionManager.Skip(1).Take(1).First();
+        tasks.Add(CheckLifetimeEnqueued(messagesPerConnection));
+        for (var i = 0; i < messagesPerConnection; i++)
+            tasks.Add(invocationConnection.InvokeAsync("SendConnection", receivingConnection.ConnectionId, $"{_message} {i}"));
+
+        await Task.WhenAll(tasks);
+        TestContext.WriteLine($"Sent/Received messages/sec: {_messageManager.LifetimeEnqueued() / (DateTime.UtcNow - startTime).TotalSeconds}");
+        Assert.AreEqual(messagesPerConnection, _messageManager.LifetimeEnqueued());
+        TestContext.WriteLine($"Elapsed ms: {(DateTime.UtcNow - startTime).TotalMilliseconds}");
+    }
+
+    [TestMethod]
+    public async Task SendConnection_MultiplePair()
+    {
+        if (ConnectionCount < 4)
+            Assert.Inconclusive("Test requires 2 connections");
+
+        const int messagesPerConnection = 10000;
+        const int pairs = ConnectionCount / 2;
+        
+        await PrimeServers();
+        var startTime = DateTime.UtcNow;
+        var tasks = new List<Task>
+        {
+            CheckLifetimeEnqueued(messagesPerConnection * pairs * 2)
+        };
+        for (var i = 0; i < pairs; i++)
+        {
+            var connectionIndex = i * 2;
+            var connection1 = _connectionManager.Skip(connectionIndex).Take(1).First();
+            var connection2 = _connectionManager.Skip(connectionIndex + 1).Take(1).First();
+            for (var j = 0; j < messagesPerConnection; j++)
+            {
+                tasks.Add(connection1.InvokeAsync("SendConnection", connection2.ConnectionId, $"{_message} 1 {j}"));
+                tasks.Add(connection2.InvokeAsync("SendConnection", connection1.ConnectionId, $"{_message} 2 {j}"));
+            }
+        }
+
+        await Task.WhenAll(tasks);
+        TestContext.WriteLine($"Sent/Received messages/sec: {_messageManager.LifetimeEnqueued() / (DateTime.UtcNow - startTime).TotalSeconds}");
+        Assert.AreEqual(messagesPerConnection * pairs * 2, _messageManager.LifetimeEnqueued());
+        TestContext.WriteLine($"Elapsed ms: {(DateTime.UtcNow - startTime).TotalMilliseconds}");
+    }
+
+    private async Task CheckLifetimeEnqueued(int expectedMessages)
+    {
+        var logTime = DateTime.UtcNow;
+        var startTime = DateTime.UtcNow;
+        
+        while (_messageManager.LifetimeEnqueued() < expectedMessages && (DateTime.UtcNow - startTime).TotalMilliseconds < ConnectionCount * RpcWait * 10)
+        {
+            if (DateTime.UtcNow - logTime >= TimeSpan.FromMilliseconds(1000))
+            {
+                TestContext.WriteLine($"messages/sec: {_messageManager.LifetimeEnqueued() / (DateTime.UtcNow - startTime).TotalSeconds}");
+                logTime = DateTime.UtcNow;
+            }
+
+            await Task.Delay(10);
+        }
     }
 }
