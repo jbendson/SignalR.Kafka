79602bc3b4a570ec33a5711d849c563db1fa347e
jbendson <jbendson@gmail.com>
2022-07-25 16:11:23 -0500

    useshellexecute

--- test/Ascentis.SignalR.Kafka.Tests/Helpers/ServerProcessHelpers.cs
+++ test/Ascentis.SignalR.Kafka.Tests/Helpers/ServerProcessHelpers.cs
@@ -14,28 +14,28 @@ public static class ServerProcessHelpers
         var workingDirectory = Path.Join(new string[] { currentDirectory, "../../../../Ascentis.SignalR.Kafka.IntegrationTests.Server/bin/Release/net6.0/" });
         var serverPath = Path.Join(new string[] { currentDirectory, "../../../../Ascentis.SignalR.Kafka.IntegrationTests.Server/bin/Release/net6.0/", "Ascentis.SignalR.Kafka.IntegrationTests.Server.exe" });
         Console.WriteLine(serverPath);
-        SetExecPrivilege("/home/runner/work/SignalR.Kafka/SignalR.Kafka/test/Ascentis.SignalR.Kafka.IntegrationTests.Server/bin/Release/net6.0/Ascentis.SignalR.Kafka.IntegrationTests.Server.dll");
+        //SetExecPrivilege("/home/runner/work/SignalR.Kafka/SignalR.Kafka/test/Ascentis.SignalR.Kafka.IntegrationTests.Server/bin/Release/net6.0/Ascentis.SignalR.Kafka.IntegrationTests.Server.dll");
 
         foreach (var port in ports)
         {
             var processStartInfo = new ProcessStartInfo
             {
                 WorkingDirectory = "/home/runner/work/SignalR.Kafka/SignalR.Kafka/test/Ascentis.SignalR.Kafka.IntegrationTests.Server/bin/Release/net6.0/",
-                FileName = "/home/runner/work/SignalR.Kafka/SignalR.Kafka/test/Ascentis.SignalR.Kafka.IntegrationTests.Server/bin/Release/net6.0/Ascentis.SignalR.Kafka.IntegrationTests.Server.dll",
-                UseShellExecute = false,
+                FileName = "Ascentis.SignalR.Kafka.IntegrationTests.Server.dll",
+                UseShellExecute = true,
                 CreateNoWindow = false,
                 Arguments = port.ToString(),
-                RedirectStandardOutput = true,
-                RedirectStandardError = true
+                //RedirectStandardOutput = true,
+                //RedirectStandardError = true
             };
 
             var server = Process.Start(processStartInfo);
-
+            /*
             server.OutputDataReceived += new DataReceivedEventHandler((sender, e) => Console.WriteLine($"[server-{port}-output] {e.Data}"));
             server.BeginOutputReadLine();
             server.ErrorDataReceived += new DataReceivedEventHandler((sender, e) => Console.WriteLine($"[server-{port}-error] {e.Data}"));
             server.BeginErrorReadLine();
-
+            */
             servers.Add(server);
         }
 
